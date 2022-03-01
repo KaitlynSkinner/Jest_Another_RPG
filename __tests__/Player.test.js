ce6aa68bf72a813b0724c69c1e85fabe4cc60275
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-02-28 19:03:03 -0500

    added player methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -17,3 +17,25 @@ test ('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+// The getStats() method will return an object containing a subset of the player's properties. 
+// The getInventory() method will return an array of Potion objects or return false if the inventory is empty.
+test("get player's stats as an object", () => {
+    const player = new Player('Jessica');
+
+    // checking that player.getStats() returns an object with four specific properties.
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Jessica');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
